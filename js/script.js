d72688d20fc9eff759edde49c5834d504715165c
09-dea <155509233+09-dea@users.noreply.github.com>
2024-01-11 08:10:54 +0100

    Projek1

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
 
 /*=============== typedjs ==========*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Fontend Developer', 'Youtuber', 'Blogger'],
+    strings: ['Pelajar Smkn 1 Majalengka'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
